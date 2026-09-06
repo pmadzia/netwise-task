65d7420030037e514537763a3896255156557766
pmadzia <piotrmadzia22@gmail.com>
2026-09-06 19:45:12 +0200

    feat: configure HTTP client and DI

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApiClient.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddHttpClient<IApiClient, ApiClient>(client =>
+{
+    client.BaseAddress = new Uri(
+        "https://catfact.ninja/");
+});
+
+using var host = builder.Build();
+
+var apiClient = host.Services.GetRequiredService<IApiClient>();
+
+var result = await apiClient.GetAsync();
+
+Console.WriteLine(result);
